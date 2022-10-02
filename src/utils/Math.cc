2aa44dc4fbbf4172a7f74162d9f5938123c67cad
Raj Mishra <rajmishra1995@gmail.com>
2022-10-02 00:27:23 -0500

    Formatting Changes

--- src/utils/Math.cc
+++ src/utils/Math.cc
@@ -3,13 +3,13 @@
 void utils::Math::multiplyMatrix(Matrix *a, Matrix *b, Matrix *c) {
     for(int i = 0; i < a->getNumRows(); i++) {
         for(int j = 0; j < b->getNumCols(); j++) {
-        for(int k = 0; k < b->getNumRows(); k++) {
-            double p      = a->getValue(i, k) * b->getValue(k, j);
-            double newVal = c->getValue(i, j) + p;
-            c->setValue(i, j, newVal);
-        }
+            for(int k = 0; k < b->getNumRows(); k++) {
+                double p      = a->getValue(i, k) * b->getValue(k, j);
+                double newVal = c->getValue(i, j) + p;
+                c->setValue(i, j, newVal);
+            }
 
-        c->setValue(i, j, c->getValue(i, j));
+            c->setValue(i, j, c->getValue(i, j));
         } 
     }
 }
